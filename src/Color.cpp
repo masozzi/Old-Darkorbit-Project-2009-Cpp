9514750521ed2f03df5bdd8a3ace0a017f005c98
Matteo Sozzi <sozziteo@googlemail.com>
2019-01-09 21:00:41 +0100

    added terminal color for linux

--- src/Color.cpp
+++ src/Color.cpp
@@ -1,7 +1,10 @@
 #include "coutcolor.h"
 
+#ifdef _WIN32
 HANDLE _ColorChange::hConsole = GetStdHandle(STD_OUTPUT_HANDLE); 
+#endif
 EColor _lastColor = EColor::DEFAULT;
+
 std::ostream& operator<<(std::ostream& os, const EColor& c)
 {
 	_ColorChange(static_cast<int>(c));
--- src/Color.h
+++ src/Color.h
@@ -1,9 +1,12 @@
 #ifndef __COLOR_H__
 #define __COLOR_H__
+
 #include <iostream>
 #include <Windows.h>
+
 enum class EColor
 {
+#ifdef _WIN32
 	BLACK,
 	DARKER_BLUE,
 	DARK_GREEN,
@@ -21,14 +24,44 @@ enum class EColor
 	PURPLE,
 	YELLOW,
 	OTHER_WHITE
+#endif
+#ifdef __linux__
+	BLACK = 30,
+	DARKER_BLUE = 94,
+	DARK_GREEN = 92,
+	DARK_TURQUOISE = 96,
+	DARK_RED = 91,
+	DARK_PURPLE = 95,
+	DARK_YELLOW = 93,
+	DEFAULT = 39,
+	WHITE = 97,
+	GREY = 90,
+	DARK_BLUE = 34,
+	GREEN = 32,
+	LIGHT_BLUE = 36,
+	RED = 31,
+	PURPLE = 35,
+	YELLOW = 33,
+	OTHER_WHITE = 37
+#endif
 };
+
 struct _ColorChange
 {
 	explicit _ColorChange(int color)
 	{
+#ifdef _WIN32
 		SetConsoleTextAttribute(hConsole, color);
+#endif
+#ifdef __linux__
+		std::cout << "\033[" << color << "m";
+#endif
 	}
+
+#ifdef _WIN32
 private:
 	static HANDLE hConsole;
+#endif
 };
+
 #endif
